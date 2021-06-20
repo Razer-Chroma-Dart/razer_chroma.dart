f0f122c4ed68de51f0c8bc94a756a5123075da30
hacker1024 <hacker1024@users.sourceforge.net>
2021-06-20 17:45:43 +1000

    feat: Export the driver and device info

--- lib/razer_chroma_macos.dart
+++ lib/razer_chroma_macos.dart
@@ -3,4 +3,6 @@ library razer_chroma_mac;
 export 'package:color/color.dart';
 
 export 'src/client.dart' hide BaseClient;
+export 'src/device_info.dart';
+export 'src/driver.dart';
 export 'src/utils/devices.dart';
